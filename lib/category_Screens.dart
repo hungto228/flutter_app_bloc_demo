0e35e56c938d06a86f088629e1e2156ee8045360
hungto228@gmail.com <hungto228@gmail.com>
2021-07-28 10:32:13 +0700

    refactor main

--- lib/category_Screens.dart
+++ lib/category_Screens.dart
@@ -5,20 +5,25 @@ import 'package:flutter_app_bloc_demo/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (catDate) => CategoryItem(
-              title: catDate.title,
-              colors: catDate.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DeliMeals"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catDate) => CategoryItem(
+                title: catDate.title,
+                colors: catDate.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
