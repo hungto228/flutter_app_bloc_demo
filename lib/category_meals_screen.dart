94b7be85f296929b46ad299f9eff63c5b77a39e1
hungto228@gmail.com <hungto228@gmail.com>
2021-07-28 16:05:06 +0700

    choose category when select meat

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,19 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_bloc_demo/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routerName = "/category-meals";
+
   @override
   Widget build(BuildContext context) {
     final routerArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routerArgs["title"];
     final catagoryId = routerArgs["id"];
+    final categoryMeal = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(catagoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: Center(
-        child: Text("the recipesFor the category !"),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeal[index].title);
+        },
+        itemCount: categoryMeal.length,
       ),
     );
   }
--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_bloc_demo/models/category_model.dart';
+import 'package:flutter_app_bloc_demo/models/meal_model.dart';
 
 const DUMMY_CATEGORIES = const [
   Category(id: "c1", title: "vietnam", color: Colors.red),
   Category(id: "c2", title: "chinese", color: Colors.yellow),
   Category(id: "c3", title: "thailan", color: Colors.blue),
+];
+
+const DUMMY_MEALS = const [
+  Meal(
+      id: "c1",
+      categories: ["c1", "c2"],
+      title: "title",
+      imageUrl:
+          "https://image.shutterstock.com/image-vector/cartoon-thumps-up-like-sign-260nw-1454100671.jpg",
+      ingredients: ["ingredients1", "ingredients2"],
+      steps: ["steps1", "steps2"],
+      duration: 20,
+      complexity: Complexity.Simple,
+      affordability: Affordability.Affordable,
+      isGlutenFree: true,
+      isVegan: true,
+      isVegatarian: false),
 ];
